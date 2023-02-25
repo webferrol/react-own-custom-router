874da5b13af1b87c01e7ad198d8e316dc1908452
webferrol <xurxo@webferrol.com>
2023-02-25 23:39:22 +0100

    Finished first refactoring

--- App.jsx
+++ App.jsx
@@ -1,35 +1,6 @@
 import { useState, useEffect } from 'react'
-
-function navigate (href) {
-  // history.pushState() anexa un registro en la sesión de historial del navegador
-  window.history.pushState({}, '', href) // No refresca la página
-
-  // Crear un evento personalizado
-  const navigationEvent = new Event(import.meta.env.VITE_PUSHSTATE)
-
-  window.dispatchEvent(navigationEvent)
-}
-
-function HomePage (props) {
-  const { pathname } = props
-  return (
-    <>
-      <h1>Home page</h1>
-      <strong>{pathname}</strong><br />
-      <button onClick={() => navigate('/about')}>About</button>
-    </>
-  )
-}
-
-function AboutPage ({ pathname }) {
-  return (
-    <>
-      <h1>About page</h1>
-      <strong>{pathname}</strong><br />
-      <button onClick={() => navigate('/')}>Home</button>
-    </>
-  )
-}
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
 
 export default function App () {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
@@ -52,8 +23,8 @@ export default function App () {
 
   return (
     <main className='app-container'>
-      {currentPath === '/' && <HomePage pathname={currentPath} />}
-      {currentPath === '/about' && <AboutPage pathname={currentPath} />}
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
     </main>
   )
 }
---
+++ pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import { Link } from '../Link'
+
+export default function HomePage () {
+  return (
+    <div>
+      HomePage
+      <Link to='/about'>About</Link>
+    </div>
+  )
+}
